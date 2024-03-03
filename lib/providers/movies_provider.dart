f843ecfe7a7b81add0925a60cee6a7b937b41e6a
YaelGF <1719110736@utectulancingo.edu.mx>
2024-03-02 22:38:42 -0600

    Peticiones HTTP

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,15 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+
+  
+  final String _apiKey = '03b81cf55660e44c8a19a04709819570';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
   
   MoviesProvider(){
-    print('MoviesProvider');
 
     this.getOnDisplayMovies();
   }
 
-  getOnDisplayMovies(){
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async{
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing',{
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1'
+    });
+
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData);
   }
 
 }
